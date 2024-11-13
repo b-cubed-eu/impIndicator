6b4bc119d6b8c2bd36e10d33acaf00c5eac483c6
Mukhtar Muhammed Yahaya <126781903+mmyahaya@users.noreply.github.com>
2024-11-13 15:13:45 +0200

    describe taxa_acacia data

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' GBIF occurrences data of acacia in South Africa
+#' An example of occurrence data from GBIF containing required column for impact indicator.
+#'
+#' @format A dataframe object containing 17,279 rows and 6variables
+#' \describe{
+#'   \item{decimalLatitude}{geographic latitude in decimal}
+#'   \item{decimalLongitude}{geographic longitude in decimal}
+#'   \item{species}{scientific name of species}
+#'   \item{speciesKey}{GBIF species identification number}
+#'   \item{coordinateUncertaintyInMeters}{radius of the uncertainty circle around geographic point}
+#'   \item{year}{year occurrence was recorded}
+#' }
+#' @examples
+#'   head(taxa_Acacia,10)
+#'
+#' @source \url{https://www.gbif.org/occurrence/search?country=ZA&taxon_key=2978223&occurrence_status=present}
+"taxa_Acacia"
